efe9eb959cd6fd3a872e05e5c6c5c5b3630b58bd
murphman300 <jeanlouis@spotit.ca>
2019-06-14 11:11:12 -0400

    Running Tests

--- cexpresser.js
+++ cexpresser.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 module.exports = async (path) => {
     if (!path) throw "Invalid Path Provided"
     try {
-        let file = fs.readFileSync(path);
-        console.log(file);
+        let buff = fs.readFileSync(path), file1 = await JSON.stringify(buff), file = await JSON.parse(file1);
         if (!file) throw "No File Returned";
         if (!file.token) throw "No Token Within File"
         return file;
